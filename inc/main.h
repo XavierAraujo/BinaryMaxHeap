c9b4d4afd0e9e607dabc9b21652784ef1aa7e2a2
Xavier Araújo <xavier.araujo92@gmail.com>
2017-09-02 20:01:53 +0100

    General doxygen documentation improvements. Added missing main.h file

---
+++ inc/main.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "binary_heap.h"
+
+/**
+ * @brief print Prints the content of a given binary heap in a form of an array. If the pointer to the heap is NULL it prints the message 'Null Heap'
+ * @param bHeap Pointer to the binary heap to be printed.
+ */
+void printHeap(binaryHeap* bHeap);
+
+/**
+ * @brief printPoppedNode Prints the content of a popped node. If the pointer to the heap is NULL it prints the message 'Null Node'
+ * @param node Pointer to the node to be printed
+ */
+void printPoppedNode(heapNode* node);
