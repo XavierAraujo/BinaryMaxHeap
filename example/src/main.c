9bda58e51a60c2d95dada701810e0ebc8f000dbc
Xavier Araújo <xa@hypelabs.io>
2018-10-18 15:45:46 +0100

    Added CMakeList to compile example source code. Fixed error on example source code

--- example/src/main.c
+++ example/src/main.c
@@ -13,19 +13,19 @@ int main()
 
     BinaryHeap* myHeap = binary_heap_create(NULL);
 
-    node.key = 11; strcpy(node.name, "Node A");
+    node.key = 11; strcpy(node.item, "Node A");
     binary_heap_push(&myHeap, node);
-    node.key = 5; strcpy(node.name, "Node B");
+    node.key = 5; strcpy(node.item, "Node B");
     binary_heap_push(&myHeap, node);
-    node.key = 8; strcpy(node.name, "Node C");
+    node.key = 8; strcpy(node.item, "Node C");
     binary_heap_push(&myHeap, node);
-    node.key = 3; strcpy(node.name, "Node D");
+    node.key = 3; strcpy(node.item, "Node D");
     binary_heap_push(&myHeap, node);
-    node.key = 4; strcpy(node.name, "Node E");
+    node.key = 4; strcpy(node.item, "Node E");
     binary_heap_push(&myHeap, node);
     printHeap(myHeap);
 
-    node.key = 15; strcpy(node.name, "Node F");
+    node.key = 15; strcpy(node.item, "Node F");
     binary_heap_push(&myHeap, node);
     printHeap(myHeap);
 
@@ -67,7 +67,7 @@ static void printPoppedNode(HeapNode* node)
         return;
     }
 
-    printf("\npoppedNode -> key:%i, name:%s\n", node->key, node->name);
+    printf("\npoppedNode -> key:%i, name:%s\n", node->key, node->item);
 }
 
 static void printHeap(BinaryHeap* bHeap)
@@ -80,5 +80,5 @@ static void printHeap(BinaryHeap* bHeap)
 
     printf("\n\n## BinaryHeap Size: %zu\n", bHeap->size);
     for(int i=0; i<bHeap->size;i++)
-        printf("[%i] -> key:%i, name:%s\n", i, bHeap->nodes[i].key, bHeap->nodes[i].name);
+        printf("[%i] -> key:%i, name:%s\n", i, bHeap->nodes[i].key, bHeap->nodes[i].item);
 }
